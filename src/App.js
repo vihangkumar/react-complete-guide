a268449295355e118be5030a08ae237e95df5e83
VDESAI <Vihang.Desai@coxautoinc.com>
2018-11-28 16:29:26 -0500

    Removing Radium code forn now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
-import Radium, {StyleRoot} from 'radium'
 
 
 class App extends Component {
@@ -59,11 +58,7 @@ class App extends Component {
         font: 'inherit', 
         border: '1px solid blue', 
         padding: '8px',
-        cursor: 'pointer',
-        ':hover': {
-          backgroundColor: 'lightgreen',
-          color: 'black'
-      }
+        cursor: 'pointer'
        }; 
 
       let persons = null
@@ -89,11 +84,7 @@ class App extends Component {
         );
 
         style.backgroundColor = 'red'; 
-        style[':hover'] = {
-          backgroundColor: 'salmon',
-          color: 'black'
-        }
-
+       
       }
 
       let classes = []; 
@@ -106,7 +97,7 @@ class App extends Component {
       }
 
     return (
-      <StyleRoot>
+      
       <div className="App">
         <h1>Hi, I'm a React App</h1>
          <p className={classes.join(' ')}>This is really working!</p>
@@ -118,7 +109,7 @@ class App extends Component {
         
 
       </div>
-      </StyleRoot>
+      
 
     );
 
@@ -128,4 +119,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
